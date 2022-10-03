03cdc406b8b80788f0c458b308bd316cc204893c
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-10-03 14:55:51 -0700

    trying my idea to standardize on double quotes in all languages since little interpolation is ever used without f now. for dual py js work all day it might be better. improving log output.

--- tf-imdb-binary-sentiment.py
+++ tf-imdb-binary-sentiment.py
@@ -9,6 +9,7 @@ import string
 import tensorflow as tf
 from keras import layers
 from keras import losses
+import pprint
 
 print(tf.__version__)
 
@@ -23,7 +24,11 @@ print(tf.__version__)
 
 
 def log(msg):
-    print(f"\n[########]    {msg}")
+    print(f"\n[####]    {msg}")
+
+
+def log_phase(msg):
+    print(f"\n[####]    ----  {msg}  ----\n")
 
 
 # TODO: IMPLEMENTING SAVING AND LOADING OF MODELS. FOLLOWING THIS:
@@ -31,9 +36,9 @@ def log(msg):
 # https://www.tensorflow.org/tutorials/keras/save_and_load#save_checkpoints_during_training
 # TODO: Doing a POC in a separate script then will adapt to this one. See: tf-save-load.py
 
-log(f"<********>  SENTIMENT ANALYSIS MODEL TRAINING & PREDICTION - IMDB MOVIE REVIEWS  <********>\n")
+log_phase(f"PROJECT:  SENTIMENT ANALYSIS MODEL TRAINING & PREDICTION - IMDB MOVIE REVIEWS")
 
-log(f"----  DOWNLOAD RAW DATA. INSPECT DIRECTORY FORMAT AND A TEXT SAMPLE  ----\n")
+log_phase(f"PHASE 1:  Download raw data. Inspect directory format and a sample text file.")
 
 url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 log(f"getting dataset .tar.gz file from: {url}")
@@ -51,19 +56,20 @@ dataset = tf.keras.utils.get_file("aclImdb_v1",
 # I am calling it the workspace_dir for now. Before I call it something else like cache_dir, I need more info.
 workspace_dir = os.path.dirname(dataset)
 
+log(f"Just have a quick look at the directory structures and then one of the text files.")
+
 log(f"workspace_dir: {workspace_dir}")
 
-dataset_dir = os.path.join(workspace_dir, 'aclImdb')
+dataset_dir = os.path.join(workspace_dir, "aclImdb")
 log(f"dataset_dir: {dataset_dir}")
+log(f"dataset_dir listing:\n\n{pprint.pprint(os.listdir(dataset_dir))}")
 
-log(f"Just have a quick look at the dir structure and then one of the files.")
-log(f"dataset_dir listing:\n\n{os.listdir(dataset_dir)}")
-
-training_dir = os.path.join(dataset_dir, 'train')
-log(f"training_dir listing:\n\n{os.listdir(training_dir)}")
+training_dir = os.path.join(dataset_dir, "train")
+log(f"training_dir: {training_dir}")
+log(f"training_dir listing:\n\n{pprint.pprint(os.listdir(training_dir))}")
 
 
-sample_file = os.path.join(training_dir, 'pos/1181_9.txt')
+sample_file = os.path.join(training_dir, "pos/1181_9.txt")
 log(f"sample file: {sample_file}\n")
 
 with open(sample_file) as f:
@@ -83,27 +89,27 @@ main_directory/
 
 # This will error on the second try because no such dir will be found.
 # Can do this only once after unpacking source data.
-log(f"Removing unsupported data from the raw dataset")
+log(f"Removing unsupported data from the raw dataset.")
 # TODO: Add an existence check or use a lib that doesn't care. (pathlib?)
 # Ideally we would download the data set once, remove this stuff once and then do training repeatedly. The original
 # code was a one-time Collab from TF docs, and it is still being adapted to something more reusable and robust.
-remove_dir = os.path.join(training_dir, 'unsup')
+remove_dir = os.path.join(training_dir, "unsup")
 shutil.rmtree(remove_dir)
 
 
-log(f"----  CREATE DATASETS  ----\n")
+log_phase(f"PHASE 2:  Create datasets.")
 
-log(f"Create RAW TRAINING DATASET from directory of text files\n")
+log(f"Create RAW TRAINING DATASET from directory of text files.\n")
 
 batch_size = 32
 seed = 42
 
 # https://www.tensorflow.org/api_docs/python/tf/keras/utils/text_dataset_from_directory
 raw_training_dataset = tf.keras.utils.text_dataset_from_directory(
-    'aclImdb/train',
+    "aclImdb/train",
     batch_size=batch_size,
     validation_split=0.2,
-    subset='training',
+    subset="training",
     seed=seed)
 
 print(f"#####]    Sample of 5:\n")
@@ -121,16 +127,16 @@ log(f"Create RAW VALIDATION DATASET from directory of text files\n")
 # In other cases, the validation data might already be in a separate directory, but not in this case. This is the
 # purpose of the validation_split option.
 raw_validation_dataset = tf.keras.utils.text_dataset_from_directory(
-    'aclImdb/train',
+    "aclImdb/train",
     batch_size=batch_size,
     validation_split=0.2,
-    subset='validation',
+    subset="validation",
     seed=seed)
 
 log(f"Create RAW TEST DATASET from directory of text files\n")
 
 raw_test_dataset = tf.keras.utils.text_dataset_from_directory(
-    'aclImdb/test',
+    "aclImdb/test",
     batch_size=batch_size)
 
 
@@ -144,8 +150,8 @@ log(f"----  CUSTOM STANDARDIZATION, TOKENIZATION, VECTORIZATION  ----\n")
 # individually.
 def custom_standardization(input_data):
     lowercase = tf.strings.lower(input_data)
-    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
-    return tf.strings.regex_replace(stripped_html, '[%s]' % re.escape(string.punctuation), '')
+    stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
+    return tf.strings.regex_replace(stripped_html, "[%s]" % re.escape(string.punctuation), "")
 
 
 max_features = 10000
@@ -154,7 +160,7 @@ sequence_length = 250
 vectorize_layer = layers.TextVectorization(
     standardize=custom_standardization,
     max_tokens=max_features,
-    output_mode='int',
+    output_mode="int",
     output_sequence_length=sequence_length)
 
 # Make a text-only dataset (without labels), then call adapt
@@ -176,7 +182,7 @@ print("Vectorized review", vectorize_text(first_review, first_label))
 
 print("1787 ---> ", vectorize_layer.get_vocabulary()[1787])
 print("2601 ---> ", vectorize_layer.get_vocabulary()[2601])
-print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
+print("Vocabulary size: {}".format(len(vectorize_layer.get_vocabulary())))
 
 training_dataset = raw_training_dataset.map(vectorize_text)
 validation_dataset = raw_validation_dataset.map(vectorize_text)
@@ -200,7 +206,7 @@ model = tf.keras.Sequential([
 model.summary()
 
 model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
-              optimizer='adam',
+              optimizer="adam",
               metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
 
 epochs = 10
@@ -220,10 +226,10 @@ log(f"----  HISTORY PLOTS  ----\n")
 history_dict = history.history
 history_dict.keys()
 
-acc = history_dict['binary_accuracy']
-val_acc = history_dict['val_binary_accuracy']
-loss = history_dict['loss']
-val_loss = history_dict['val_loss']
+acc = history_dict["binary_accuracy"]
+val_acc = history_dict["val_binary_accuracy"]
+loss = history_dict["loss"]
+val_loss = history_dict["val_loss"]
 
 epochs = range(1, len(acc) + 1)
 
@@ -231,12 +237,12 @@ epochs = range(1, len(acc) + 1)
 log(f"PLOT: Training Loss, Validation Loss\n")
 
 # "bo" is for "blue dot"
-plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, loss, "bo", label="Training loss")
 # b is for "solid blue line"
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
 plt.legend()
 
 plt.show()
@@ -244,29 +250,29 @@ plt.show()
 
 log(f"PLOT: Training Accuracy, Validation Accuracy\n")
 
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend(loc='lower right')
+plt.plot(epochs, acc, "bo", label="Training acc")
+plt.plot(epochs, val_acc, "b", label="Validation acc")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(loc="lower right")
 
 plt.show()
 
 
-log(f"----  TEST PHASE  ----\n")
+log_phase(f"PHASE 4: Export and test phase. Test the exported model using RAW TEST DATASET.")
 
 log(f"tf.keras.Sequential groups a linear stack of layers into a tf.keras.Model.")
 log(f"vectorize_layer passed in, model passed in, layers.Activation: sigmoid. Obj name: export_model.")
 export_model = tf.keras.Sequential([
     vectorize_layer,
     model,
-    layers.Activation('sigmoid')
+    layers.Activation("sigmoid")
 ])
 
 log(f"export_model.compile - losses.BinaryCrossentropy-non-logits, optimizer: adam")
 export_model.compile(
-    loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
+    loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=["accuracy"]
 )
 
 log(f"export_model.evaluate raw_test_dataset")
@@ -275,7 +281,7 @@ loss, accuracy = export_model.evaluate(raw_test_dataset)
 log(f"Accuracy:\n{accuracy}")
 
 
-log(f"----  PREDICTION  ----\n")
+log_phase(f"PHASE 5:  Prediction. Running the exported model.")
 
 examples = [
     "The movie was great!",
@@ -291,7 +297,8 @@ result = export_model.predict(examples)
 
 # In Collab, you would see the .predict() output automatically, but here we have to explicitly print it.
 print(examples)
+print()
 print(result)
 
-log(f"--  SENTIMENT ANALYSIS TENSORFLOW/KERAS DEMONSTRATION COMPLETE.  Exiting.\n")
+log_phase(f"PROJECT:  SENTIMENT ANALYSIS TENSORFLOW/KERAS DEMONSTRATION COMPLETE.  Exiting.")
 
